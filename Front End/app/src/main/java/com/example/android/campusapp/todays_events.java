21573436a3f6d37c7bf870e40f968163ded49b4f
elsabergman <elsabergman@nl119-180-121.student.uu.se>
2017-03-31 11:00:35 +0200

    nya filer

---
+++ Front End/app/src/main/java/com/example/android/campusapp/todays_events.java
@@ -0,0 +1,17 @@
+package com.example.android.campusapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by elsabergman on 2017-03-31.
+ */
+
+public class todays_events  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.todays_events);
+    }
+}
